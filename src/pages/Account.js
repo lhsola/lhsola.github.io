1836008806a21c80763e994527f7681a21884f95
lhsola <lhsola9542@gmail.com>
2025-11-11 13:58:22 +0900

    test
    test

--- src/pages/Account.js
+++ src/pages/Account.js
@@ -35,14 +35,14 @@ function ContactButton({ person, account, kakaopay }) {
   function Account() {
     const groom_contact = [
       { person: "신랑 김신랑", account: "우리은행 1002-123-456789", kakaopay: "" },
-      { person: "아버지 김아빠", account: "우리은행 0123456789", kakaopay: "" },
-      { person: "어머니 박엄마", account: "농협은행 0123456789", kakaopay: "" },
+      // { person: "아버지 김아빠", account: "우리은행 0123456789", kakaopay: "" },
+      // { person: "어머니 박엄마", account: "농협은행 0123456789", kakaopay: "" },
     ];
 
     const bride_contact = [
         { person: "신부 이신부", account: "토스뱅크 1000-0123-4567" , kakaopay: "" },
-        { person: "아버지 이아빠", account: "기업은행 0123456789", kakaopay: ""},
-        { person: "어머니 우엄마", account: "국민은행 0123456789", kakaopay: "" },
+        // { person: "아버지 이아빠", account: "기업은행 0123456789", kakaopay: ""},
+        // { person: "어머니 우엄마", account: "국민은행 0123456789", kakaopay: "" },
       ];
   
     return (
