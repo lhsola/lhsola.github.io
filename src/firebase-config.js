a5333a52dc3f7b5f5468bd4f5593eb8268e50317
lhsola <lhsola9542@gmail.com>
2025-11-14 11:35:35 +0900

    test
    test

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,29 +1,7 @@
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// import { getFirestore } from "firebase/firestore";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-// // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDHaoVL3mi4_PFRxbr91cQOJ8SvRkqNbLg",
-//   authDomain: "wedding-invitation-5e3c5.firebaseapp.com",
-//   projectId: "wedding-invitation-5e3c5",
-//   storageBucket: "wedding-invitation-5e3c5.appspot.com",
-//   messagingSenderId: "288046083919",
-//   appId: "1:288046083919:web:7621d047d94c49d8059f3a",
-//   measurementId: "G-KP677M2TTQ"
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const db = getFirestore(app);
-
 // Import the functions you need from the SDKs you need
 import { getFirestore } from "firebase/firestore";
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+// import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -41,7 +19,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
 const db = getFirestore(app);
 
 export default db;
