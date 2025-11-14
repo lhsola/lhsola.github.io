e27fc6bc1b1c0efb790df1e274a1ef25e63adb98
lhsola <lhsola9542@gmail.com>
2025-11-14 11:11:32 +0900

    test
    test

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -21,6 +21,7 @@
 // const db = getFirestore(app);
 
 // Import the functions you need from the SDKs you need
+import { getFirestore } from "firebase/firestore";
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
