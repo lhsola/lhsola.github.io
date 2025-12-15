d359acc9152dd6d7953905f9cb9cd593500e3fdb
lhsola <lhsola9542@gmail.com>
2025-12-15 15:49:18 +0900

    test
    test

--- src/pages/Cover.js
+++ src/pages/Cover.js
@@ -1,7 +1,5 @@
 import React, { useState, useRef, useEffect } from 'react'
 import mainPhoto from '../images/photo.jpg'
-import { TbPlayerTrackPrevFilled, TbPlayerSkipBackFilled, TbPlayerSkipForwardFilled, TbPlayerTrackNextFilled } from "react-icons/tb";
-import { BsPlayCircle, BsStopCircle } from "react-icons/bs";
 import { GoHeartFill } from "react-icons/go";
 import myMusic from '../media/taeyeon_poem.mp3';
 
@@ -9,25 +7,9 @@ function Cover() {
 
   const [isPlaying, setIsPlaying] = useState(false);
   const audioRef = useRef(new Audio(myMusic));
-
   const togglePlay = () => {
     setIsPlaying(!isPlaying);
   };
-
-  // 모바일 100vh 문제 해결 로직
-//  useEffect(() => {
-//   const setVh = () => {
-//     const vh = window.innerHeight * 0.01;
-//     document.documentElement.style.setProperty('--vh', `${vh}px`);
-//   };
-
-//   setVh();
-
-//   window.addEventListener('resize', setVh);
-//   return () => window.removeEventListener('resize', setVh);
-// }, []);
-
-
 
   useEffect(() => {
     const audio = audioRef.current;
@@ -49,44 +31,13 @@ function Cover() {
             <div>최명현</div>
             {/* Heart 아이콘에 fill 속성을 사용하여 색상 적용 */}
            <GoHeartFill className='cover__icon-heart' size="0.8em"/>
-            <div>혜선</div>
+            <div>이혜선</div>
           </div>
           <div className='cover__date'>2026년 4월 25일 토요일 12시</div>
           <div className='cover__place'>서초 디토레스토랑</div>          
         </div>
       </div>
-
   )
-  // return (
-  //   <div className="cover__container">
-  //     {/* <img className="cover__main-photo" src={mainPhoto} alt="weddingcouple" /> */}
-  //     <img src={mainPhoto} alt="weddingcouple" />
-  //     <div className="cover__overlay">
-  //       <div className='cover__person'>
-  //         <div>최명현</div>
-  //         <GoHeartFill className='cover__icon-heart' size="0.8em"/>
-  //         <div>이혜선</div>
-  //       </div>
-  //       <div className='cover__date'>2026년 4월 25일 토요일 12시</div>
-  //       <div className='cover__place'>서초 디토레스토랑</div>
-  //       {/* <div className='cover__line'></div>  */}
-  //        <div className='cover__icon-box'>
-  //         {/* <TbPlayerTrackPrevFilled size="1.5em"/> */}
-  //         {/* <TbPlayerSkipBackFilled size="1.5em"/> */}
-  //         {/* {isPlaying ? (
-  //           <BsStopCircle size="3em" className='cover__music-btn' onClick={togglePlay} aria-label="Stop Music"/>
-  //         ) : (
-  //           <BsPlayCircle size="3em" className='cover__music-btn' onClick={togglePlay} aria-label="Play Music"/>
-  //         )} */}
-  //         {/* <TbPlayerSkipForwardFilled size="1.5em"/> */}
-  //         {/* <TbPlayerTrackNextFilled size="1.5em"/> */}
-  //       </div> 
-  //     </div>
-  //   </div>
-  // )
-
-
-
 }
 
 export default Cover
--- src/pages/Invitation.js
+++ src/pages/Invitation.js
@@ -33,7 +33,7 @@ function Invitation() {
           <div><span className="invitation__initial">명</span>백해진 마음으로</div>
           <div><span className="invitation__initial">현</span>명한 선택을 마주한 두 사람이</div>
           {/* 현명한 선택 앞에 서로를 마주하고 선택을 마주한 두 사람이 */}
-          <div><span className="invitation__initial">혜</span>심으로 다름을 품으며</div>
+          <div><span className="invitation__initial">혜</span>심으로 서로다름을 품으며</div>
           <div><span className="invitation__initial">선</span>택한 길을 함께 걷고자 합니다.</div>
           <div>두 사람의 새로운 시작을 </div>
           <div>함께 축복해주시면 감사하겠습니다. </div>
