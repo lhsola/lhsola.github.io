02a73fbcd85f9a9c25301983330eac49e71efeeb
lhsola <lhsola9542@gmail.com>
2026-01-07 11:51:20 +0900

    test
    test

--- src/components/ShareButton.js
+++ src/components/ShareButton.js
@@ -24,7 +24,7 @@ function ShareButton() {
       content: {
         title: "명현❤️혜선 결혼식에 초대합니다.",
         description: "4월 25일 12시 <서초 디토레스토랑>\n서울 서초구 명달로 94",
-        imageUrl: "https://lhsola.github.io/main.jpg", // ✅ 썸네일 이미지 주소
+        imageUrl: "https://mhs260425.web.app/main.jpg", // ✅ 썸네일 이미지 주소
         link: {
           mobileWebUrl: "https://mhs260425.web.app/",
           webUrl: "https://mhs260425.web.app/",
